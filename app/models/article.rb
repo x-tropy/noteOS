cd06d4c569135ba68c22ce15d38417bef9ca05da
x-tropy <buweiliao@gmail.com>
2024-09-28 22:51:56 +0200

    update model Article (friendly_id)

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,8 @@
 class Article < ApplicationRecord
+  extend FriendlyId
   belongs_to :author, class_name: 'User'
   has_rich_text :content
   has_paper_trail
   has_many :comments
+  friendly_id :title, use: :slugged
 end
