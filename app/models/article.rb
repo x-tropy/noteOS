3c22efe14fec0ad54481c32412084e2ad32aca61
x-tropy <buweiliao@gmail.com>
2024-09-29 12:33:31 +0200

    add friendly_id, has_paper_trail in article model user has_many articles, has_many comments

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,8 +1,9 @@
 class Article < ApplicationRecord
   extend FriendlyId
+  friendly_id :title, use: :slugged
+  
   belongs_to :author, class_name: 'User'
   has_rich_text :content
   has_paper_trail
-  has_many :comments
-  friendly_id :title, use: :slugged
+  has_many :comments, dependent: :destroy
 end
