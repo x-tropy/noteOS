1afac539aef3f38eb3a864c6540bb48b88d18149
x-tropy <buweiliao@gmail.com>
2024-10-04 12:06:30 +0200

    transform into setup function style

--- app/javascript/components/Tiptap.vue
+++ app/javascript/components/Tiptap.vue
@@ -3,15 +3,23 @@
 </template>
 
 <script setup>
-import { useEditor, EditorContent } from "@tiptap/vue-3";
-import StarterKit from "@tiptap/starter-kit";
-import { onMounted, onBeforeUnmount, ref } from "vue";
+import { ref, onMounted, onBeforeUnmount } from 'vue'
+import { Editor, EditorContent } from '@tiptap/vue-3'
+import StarterKit from '@tiptap/starter-kit'
 
+// Create a ref to hold the editor instance
+const editor = ref(null)
 
 onMounted(() => {
-  const editor = useEditor({
-    content: "<p>I’m running Tiptap with Vue.js. 🎉</p>",
+  // Initialize the editor when the component is mounted
+  editor.value = new Editor({
+    content: '<p>I’m running Tiptap with Vue.js. 🎉</p>',
     extensions: [StarterKit],
-  });
-});
+  })
+})
+
+onBeforeUnmount(() => {
+  // Destroy the editor when the component is unmounted
+  editor.value.destroy()
+})
 </script>
