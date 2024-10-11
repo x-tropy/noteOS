7e3d5eff9381eecdf4770877df8e48a117888734
x-tropy <buweiliao@gmail.com>
2024-10-11 20:35:27 +0200

    undo: doesn't work

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,10 +21,10 @@ Rails.application.configure do
   # config.require_master_key = true
 
   # Disable serving static files from `public/`, relying on NGINX/Apache to do so instead.
-  config.public_file_server.enabled = true
-  config.public_file_server.headers = {
-    "Cache-Control" => "public, max-age=31536000, immutable"
-  }
+  # config.public_file_server.enabled = true
+  # config.public_file_server.headers = {
+  #   "Cache-Control" => "public, max-age=31536000, immutable"
+  # }
 
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
