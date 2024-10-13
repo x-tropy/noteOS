e0f799d560e493c0253c950fbd7df489adf7edb0
x-tropy <buweiliao@gmail.com>
2024-10-13 21:28:09 +0200

    cache: use CDN to serve js, css

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,7 +33,7 @@ Rails.application.configure do
   config.assets.compile = false
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  # config.asset_host = "bucket.noteos.org/public/"
+  config.asset_host = "fly.storage.tigris.dev/static-noteos/"
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for Apache
