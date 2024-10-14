157dd2e2ad9c2fbd0ab385975bda75184983f013
x-tropy <buweiliao@gmail.com>
2024-10-14 17:35:15 +0200

    set session context with `ActiveStorage::SetCurrent`

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :set_item, only: %i[ show edit update destroy ]
+  include ActiveStorage::SetCurrent
 
   # GET /items or /items.json
   def index
