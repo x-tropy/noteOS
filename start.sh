b4914ab80b255a78e016c442250207573d4dd811
x-tropy <buweiliao@gmail.com>
2024-10-17 21:18:52 +0200

    fix bad gateway error

--- start.sh
+++ start.sh
@@ -3,10 +3,5 @@
 # Start Rails server
 ./bin/rails server -b 127.0.0.1 -p 5000 &
 
-# Wait until Rails server is ready before starting Nginx
-while ! nc -z 127.0.0.1 5000; do
-  sleep 0.1
-done
-
 # Start Nginx
 nginx -c $(pwd)/nginx.conf
