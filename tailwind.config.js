a194e1e76b77912f9a65bd2d47c515bbe92cba59
x-tropy <buweiliao@gmail.com>
2024-10-20 12:46:52 +0200

    style: add shadow-elevation preset

--- tailwind.config.js
+++ tailwind.config.js
@@ -32,7 +32,18 @@ module.exports = {
         "monospace",
       ],
     },
-    extend: {},
+    extend: {
+      boxShadow: {
+        'elevation-xs': '0 0 0 1px rgba(17,20,24,.15)',
+        'elevation-sm': '0 0 0 1px rgba(17,20,24,.1),0 1px 1px rgba(17,20,24,.2)',
+        'elevation': '0 0 0 1px rgba(17,20,24,.1),0 1px 1px rgba(17,20,24,.2),0 2px 6px rgba(17,20,24,.2)',
+        'elevation-md': '0 0 0 1px rgba(17,20,24,.1),0 2px 4px rgba(17,20,24,.2),0 8px 24px rgba(17,20,24,.2)',
+        'elevation-lg': '0 0 0 1px rgba(17,20,24,.1),0 4px 8px rgba(17,20,24,.2),0 18px 46px 6px rgba(17,20,24,.2)',
+      }
+    },
+  },
+  variants: {
+    boxShadow: ['hover', 'active'], // Enable hover and active states for box-shadow
   },
   corePlugins: {
     aspectRatio: false,
