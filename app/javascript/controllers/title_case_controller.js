1cb8d5d8229cd1fe4a7612a4983d5aa0234a66c0
x-tropy <buweiliao@gmail.com>
2024-10-20 12:46:07 +0200

    code style: improve title_case_controller

--- app/javascript/controllers/title_case_controller.js
+++ app/javascript/controllers/title_case_controller.js
@@ -1,17 +1,20 @@
-import {titleCase} from "title-case";
-import { Controller } from "@hotwired/stimulus"
+import { titleCase } from "title-case";
+import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-    static targets = ["title"]
-    connect() {
-    }
+  static targets = ["title"];
 
-    handleBlur() {
-        const titleValue = this.titleTarget.value;
-        const newTitle = titleCase((titleValue))
-        const firstH1 = document.querySelector(".tiptap[contenteditable] h1")
-        if (firstH1.innerText.trim() === "") {
-            firstH1.innerText = newTitle
-        }
+  connect() {}
+
+  handleBlur() {
+    const newTitle = titleCase(this.title);
+    const h1 = document.querySelector(".tiptap[contenteditable] h1");
+    if (h1.innerText.trim() === "") {
+      h1.innerText = newTitle;
     }
+  }
+
+  get title() {
+    return this.titleTarget.value;
+  }
 }
