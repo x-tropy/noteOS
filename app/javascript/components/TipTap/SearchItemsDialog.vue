f47d7f35c295517d2912d1927266ab7a76e72a9b
x-tropy <buweiliao@gmail.com>
2024-10-29 18:19:49 +0100

    feature: add search items dialog (not done yet)

---
+++ app/javascript/components/TipTap/SearchItemsDialog.vue
@@ -0,0 +1,13 @@
+<script setup>
+import {IconFileSearch} from "@tabler/icons-vue"
+</script>
+
+<template>
+  <button @click.prevent="" class="focus:outline-none">
+    <IconFileSearch />
+  </button>
+</template>
+
+<style scoped>
+
+</style>
--- app/javascript/components/TipTap/Toolbar.vue
+++ app/javascript/components/TipTap/Toolbar.vue
@@ -15,7 +15,8 @@ import {
 } from "@tabler/icons-vue";
 import Menu from "primevue/menu";
 import AddImagePopover from "~/components/TipTap/AddImagePopover.vue";
-import AttachFileDialog from "@/components/TipTap/AttachFileDialog.vue";
+import AttachFileDialog from "~/components/TipTap/AttachFileDialog.vue";
+import SearchItemsDialog from "~/components/TipTap/SearchItemsDialog.vue";
 
 const items = ref([
   {
@@ -38,7 +39,6 @@ const props = defineProps({
     required: true,
   },
 });
-
 </script>
 
 <template>
@@ -48,9 +48,9 @@ const props = defineProps({
         <IconLink :class="{ 'bg-black': editor.isActive('link') }" />
       </button>
       <button
-          @click.prevent="unsetLink(editor)"
-          :disabled="!editor.isActive('link')"
-          :class="{ 'text-gray-400': !editor.isActive('link') }"
+        @click.prevent="unsetLink(editor)"
+        :disabled="!editor.isActive('link')"
+        :class="{ 'text-gray-400': !editor.isActive('link') }"
       >
         <IconUnlink />
       </button>
@@ -58,19 +58,19 @@ const props = defineProps({
     <div class="button-group">
       <button @click.prevent="alignLeft(editor)">
         <IconAlignLeft2
-            :class="{ 'bg-black': editor.isActive({ textAlign: 'left' }) }"
+          :class="{ 'bg-black': editor.isActive({ textAlign: 'left' }) }"
         />
       </button>
       <button @click.prevent="alignCenter(editor)">
         <IconAlignCenter
-            :class="{ 'bg-black': editor.isActive({ textAlign: 'center' }) }"
+          :class="{ 'bg-black': editor.isActive({ textAlign: 'center' }) }"
         />
       </button>
     </div>
     <div class="button-group">
       <AddImagePopover :editor="editor" />
       <AttachFileDialog :editor="editor" />
-      <SearchItemsDialog :editor="editor"/>
+      <SearchItemsDialog :editor="editor" />
     </div>
   </div>
   <Menu ref="menu" id="overlay_menu" :model="items" :popup="true" />
