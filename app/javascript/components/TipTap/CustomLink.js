fc594bdbbed1cd8381d931d341f8c152bbfb61c7
x-tropy <buweiliao@gmail.com>
2024-11-07 10:49:13 +0100

    augment: customize link to enable openOnClick when holding Ctrl in edit mode

--- app/javascript/components/TipTap/CustomLink.js
+++ app/javascript/components/TipTap/CustomLink.js
@@ -1,10 +1,28 @@
 import Link from "@tiptap/extension-link";
+import { Plugin } from "prosemirror-state";
 
 export default Link.configure({
-    openOnClick: false,
-    defaultProtocol: "https",
-    autolink: true,
-    linkOnPaste: true,
+  openOnClick: false,
+  defaultProtocol: "https",
+  autolink: true,
+  linkOnPaste: true,
 }).extend({
-    exitable: true,
-})
+  exitable: true,
+  addProseMirrorPlugins() {
+    return [
+      new Plugin({
+        props: {
+          handleDOMEvents: {
+            mousedown: (view, event) => {
+              if (event.target.tagName === "A" && event.ctrlKey) {
+                window.open(event.target.href, "_blank");
+                return true;
+              }
+              return false;
+            },
+          },
+        },
+      }),
+    ];
+  },
+});
