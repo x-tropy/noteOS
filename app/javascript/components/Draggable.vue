1735358d2cebb7d2fcf47d8ec63234d76b8df6fe
x-tropy <buweiliao@gmail.com>
2024-11-12 17:55:51 +0100

    boilerplate: vuedraggable

---
+++ app/javascript/components/Draggable.vue
@@ -0,0 +1,20 @@
+<script setup>
+import draggable from "vuedraggable";
+import { ref } from "vue";
+
+const myArray = ref([
+  { id: 1, name: "Alice" },
+  { id: 2, name: "Bob" },
+  { id: 3, name: "Charlie" },
+  { id: 4, name: "Daisy" },
+  { id: 5, name: "Ethan" },
+]);
+</script>
+
+<template>
+  <draggable v-model="myArray" item-key="id">
+    <template #item="{ element }">
+      <div>{{ element.name }}</div>
+    </template>
+  </draggable>
+</template>
